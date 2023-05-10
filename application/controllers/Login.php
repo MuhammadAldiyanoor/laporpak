db12fea01ec815c1f47229b062f977f936ee5993
MuhammadAldiyanoor <Mhmmdaldi877@gmail.com>
2023-05-10 17:15:54 +0800

    update back and

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -39,6 +39,52 @@ class Login extends CI_Controller
             }
         }
     }
+    public function index()
+	{
+		$this->_rules();
+
+		if ($this->form_validation->run() == FALSE) {
+			$data['title'] = "SIMABAR - LOGIN";
+			$this->load->view('templates_admin/header', $data);
+			$this->load->view('login', $data);
+		} else {
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			
+			$cek = $this->M_akun->cek_login($username, $password);
+
+			if ($cek == FALSE) {
+				$this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+				<strong>Username atau Password salah !</strong>
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			  	<span aria-hidden="true">&times;</span>
+				</button>
+		  		</div>');
+				redirect('login');
+			} else {
+				$this->session->set_userdata('id_level', $cek->id_level);
+				$this->session->set_userdata('nama', $cek->nama);
+				$this->session->set_userdata('nama_ruangan', $cek->nama_ruangan);
+				$this->session->set_userdata('id_ruangan', $cek->id_ruangan);
+				$this->session->set_userdata('nip', $cek->nip);
+				$this->session->set_userdata('foto', $cek->foto);
+				$this->session->set_userdata('id_akun', $cek->id_akun);
+				switch ($cek->id_level) {
+					case 1:
+						redirect('admin/dashboard');
+						break;
+					case 2:
+						redirect('petugas/dashboard');
+						break;
+					case 3:
+						redirect('user/dashboard');
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
 
 	public function logout()
 	{
--- application/models/M_akun.php
+++ application/models/M_akun.php
@@ -5,4 +5,21 @@ class M_akun extends CI_Model
     {
         return $this->db->get_where('user', $data);
     }
+    public function cek_login()
+    {
+        $username   = set_value('username');
+        $password   = set_value('password');
+
+        $result     = $this->db->where('username', $username)
+            ->where('password', md5($password))
+            ->join('tb_ruangan', 'tb_ruangan.id_ruangan=tb_akun.id_ruangan')
+            ->limit(1)
+            ->get('tb_akun');
+
+        if ($result->num_rows() > 0) {
+            return $result->row();
+        } else {
+            return FALSE;
+        }
+    }
 }
